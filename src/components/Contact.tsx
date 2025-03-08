472cf22d3789d7d9a395e87ccf95f3e0a81dba5c
Steven Edward <stevenedward@Stevens-MacBook-Air.local>
2025-03-09 01:22:20 +0800

    up

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,9 +3,9 @@ import { useTheme } from '../ThemeContext';
 import emailjs from "@emailjs/browser";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import mark from '/marker-icon.png';
-import mark2x from '/marker-icon-2x.png';
-import markshadow from '/marker-shadow.png';
+import mark from "/public/marker-icon.png";
+import mark2x from "/public/marker-icon-2x.png";
+import markshadow from "/public/marker-shadow.png";
 
 // Define Google Maps types if not already available
 declare global {
